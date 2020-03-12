2da637bed47341d08e877189c0c9a5f5ecf3c81f
Ondra <ondys.kubicek@gmail.com>
2020-03-12 13:50:07 +0100

    entity book review and test

---
+++ api/src/Entity/Test.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ApiResource
+ * @ORM\Entity
+ */
+class Test
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column
+     * @Assert\NotBlank
+     */
+    public $name = '';
+
+    /**
+     * @var string
+     *
+     * @ORM\Column
+     * @Assert\NotBlank
+     */
+    public $text = '';
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+}
